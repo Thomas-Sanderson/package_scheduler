10df1f6b1966066cbc9c8bbef972a32964eeeaa0
Nate Brennnand <natebrennand@gmail.com>
2013-04-20 18:19:53 -0400

    Mock allotments per hour

---
+++ allotment.py
@@ -0,0 +1,18 @@
+"""
+A mock template of how many reservations would be allotted per timeslot.
+"""
+
+allotment = {
+'09' : 8,
+'10' : 7,
+'11' : 4,
+'12' : 2,
+'13' : 0,
+'14' : 0,
+'15' : 0,
+'16' : 2,
+'17' : 5,
+'18' : 7,
+'19' : 9,
+'20' : 10,
+}
