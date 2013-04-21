d05f4de53d9c7fbb129c768b180ea55448930755
Nate Brennnand <natebrennand@gmail.com>
2013-04-21 02:49:06 -0400

    Dropping redundant table

--- setup_db.py
+++ setup_db.py
@@ -12,22 +12,11 @@ def setup():
 	    PRIMARY KEY (card_num)
 	)"""
 
-	log = """
-	CREATE TABLE IF NOT EXISTS log (
-	    card_num    INTEGER,    -- 9 digit card number
-	    apt_date    TEXT,       -- http://www.sqlite.org/lang_datefunc.html
-	    hour		INTEGER,
-	    minute		INTEGER,		-- 0, 15, 30 or 45
-	    PRIMARY KEY (card_num)
-	)"""
 
 	db = lite.connect('data.db')
 	c = db.cursor()
 
 	c.execute(appointment)
-	db.commit()
-
-	c.execute(log)
 	db.commit()
 
 	c.close()
