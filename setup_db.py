a5f07b1c333c403f30937bbaf55b3ed62bfa9191
Nate Brennnand <natebrennand@gmail.com>
2013-04-21 02:56:43 -0400

    Added auto generated unique id's

--- setup_db.py
+++ setup_db.py
@@ -5,11 +5,11 @@ def setup():
 
 	appointment = """
 	CREATE TABLE IF NOT EXISTS appointment (
+        id          INTEGER PRIMARY KEY AUTOINCREMENT,
 	    card_num    INTEGER,    -- 9 digit card number
 	    apt_date    TEXT,       -- http://www.sqlite.org/lang_datefunc.html
 	    hour		INTEGER,
-	    minute		INTEGER,		-- 0, 15, 30 or 45
-	    PRIMARY KEY (card_num)
+	    minute		INTEGER		-- 0, 15, 30 or 45
 	)"""
 
 
