2c6f1dc8929a0578e0cb4989638c18e5b3c146de
Nate Brennand <natebrennand@gmail.com>
2013-05-31 00:49:34 -0700

    fake data for testing

---
+++ config/allotment.py
@@ -0,0 +1,66 @@
+"""
+fake data for testing purposes
+"""
+
+# midnight - 8 AM
+# 9 AM - 4 PM
+# 5 PM - 11 PM
+
+current_week = [  # Monday
+    [None, None, None, None, None, None, None, None,
+     8,  7,  4,  2,  0,  0,  0,  2,
+     5,  7,  9,  9,  None, None, None, None],
+    # Tuesday
+    [None, None, None, None, None, None, None, None,
+     8,  7,  4,  2,  0,  0,  0,  2,
+     5,  7,  9,  9,  None, None, None, None],
+    # Wednesday
+    [None, None, None, None, None, None, None, None,
+     8,  7,  4,  2,  0,  0,  0,  2,
+     5,  7,  9,  9,  None, None, None, None],
+    # Thursday
+    [None, None, None, None, None, None, None, None,
+     8,  7,  4,  2,  0,  0,  0,  2,
+     5,  7,  9,  9,  None, None, None, None],
+    # Friday
+    [None, None, None, None, None, None, None, None,
+     8,  7,  4,  2,  0,  0,  0,  2,
+     5,  7,  9,  9,  None, None, None, None],
+    # Saturday
+    [None, None, None, None, None, None, None, None,
+     None, None, None, None, None, None, None, None,
+     None, None, None, None, None, None, None, None],
+    # Sunday
+    [None, None, None, None, None, None, None, None,
+     None, None, None, None, None, None, None, None,
+     None, None, None, None, None, None, None, None]]
+
+next_week = [  # Monday
+    [None, None, None, None, None, None, None, None,
+     8,  7,  4,  2,  0,  0,  0,  2,
+     5,  7,  9,  9,  None, None, None, None],
+    # Tuesday
+    [None, None, None, None, None, None, None, None,
+     8,  7,  4,  2,  0,  0,  0,  2,
+     5,  7,  9,  9,  None, None, None, None],
+    # Wednesday
+    [None, None, None, None, None, None, None, None,
+     8,  7,  4,  2,  0,  0,  0,  2,
+     5,  7,  9,  9,  None, None, None, None],
+    # Thursday
+    [None, None, None, None, None, None, None, None,
+     8,  7,  4,  2,  0,  0,  0,  2,
+     5,  7,  9,  9,  None, None, None, None],
+    # Friday
+    [None, None, None, None, None, None, None, None,
+     8,  7,  4,  2,  0,  0,  0,  2,
+     5,  7,  9,  9,  None, None, None, None],
+    # Saturday
+    [None, None, None, None, None, None, None, None,
+     None, None, None, None, None, None, None, None,
+     None, None, None, None, None, None, None, None],
+    # Sunday
+    [None, None, None, None, None, None, None, None,
+     None, None, None, None, None, None, None, None,
+     None, None, None, None, None, None, None, None]]
+
