968b211161a643e87b2a4d49a741f650216f1c57
Nate Brennand <natebrennand@gmail.com>
2013-05-31 00:48:34 -0700

    Adding skeleton file for appointment slots

---
+++ config/allotment_skeleton.py
@@ -0,0 +1,66 @@
+"""
+A mock template of how many reservations would be allotted per timeslot.
+"""
+
+
+# midnight - 8 AM
+# 9 AM - 4 PM
+# 5 PM - 11 PM
+
+current_week = [ # Monday
+    [0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0],
+    # Tuesday
+    [0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0],
+    # Wednesday
+    [0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0],
+    # Thursday
+    [0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0],
+    # Friday
+    [0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0],
+    # Saturday
+    [None, None, None, None, None, None, None, None,
+     None, None, None, None, None, None, None, None,
+     None, None, None, None, None, None, None, None],
+    # Sunday
+    [None, None, None, None, None, None, None, None,
+     None, None, None, None, None, None, None, None,
+     None, None, None, None, None, None, None, None]]
+
+next_week = [ # Monday
+    [0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0],
+    # Tuesday
+    [0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0],
+    # Wednesday
+    [0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0],
+    # Thursday
+    [0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0],
+    # Friday
+    [0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0],
+    # Saturday
+    [None, None, None, None, None, None, None, None,
+     None, None, None, None, None, None, None, None,
+     None, None, None, None, None, None, None, None],
+    # Sunday
+    [None, None, None, None, None, None, None, None,
+     None, None, None, None, None, None, None, None,
+     None, None, None, None, None, None, None, None]]
