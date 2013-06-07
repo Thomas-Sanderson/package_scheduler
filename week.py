66ddbab301b95faa6d96eac0b33d35ac4b59c1b9
Nate Brennand <natebrennand@gmail.com>
2013-06-06 23:25:20 -0400

    adjusts counts to reflect reservations

--- week.py
+++ week.py
@@ -1,14 +1,19 @@
 
 from config import allotment
-from datetime import date
+from datetime import date as dt_date
+import queries
 
 
 class Week:
 
-    def __init__(self, appointments=None):
+    def __init__(self, appointments):
+        """ Appointments is an array of the # of appointments for each
+            available time slot.
+            Starts with tomorrow.
+        """
         week_array = []
-        self.now = date.today().weekday()
 
+        # creating array of appointments
         for i in range(7):      # first week
             week_array.append([])
             for j in range(24):
@@ -22,7 +27,18 @@ class Week:
                 for k in range(4):
                     week_array[i][j].append(allotment.next_week[i % 7][j])
 
+        # limiting array to only the next 7 days
+        self.now = dt_date.today().weekday()
         self.slots = week_array[self.now:self.now+7]
+
+        # inserting appointments into the array for the week
+        for date, time, num in appointments:
+            day = (date-dt_date.today()).days-1
+            hour = time.hour
+            minute = time.minute/15
+            if self.slots[day][hour][minute] != None:
+                self.slots[day][hour][minute] -= int(num)
+
 
     def __str__(self):
         week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday",
