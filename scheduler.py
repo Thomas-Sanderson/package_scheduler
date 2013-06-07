eed83d1bffb11e30a564b7c2f93339dd15f83ffd
Nate Brennand <natebrennand@gmail.com>
2013-06-06 23:27:58 -0400

    update db config for postgres

--- scheduler.py
+++ scheduler.py
@@ -1,35 +1,50 @@
 from flask import Flask, render_template, request, flash, \
     url_for, g, redirect
-from allotment import allotment
-import db_config
 
-num_days = 5
+from config.allotment import current_week, next_week
+import queries
 
+from os import getenv
+import psycopg2
 
 app = Flask(__name__)
-db_conn = 'postgresssql+psycopg2://{}:{}@{}/{}'.format(
-            db_config.username, db_config.password, db_config.host, db_config.db);
+app.config.from_object('config.app_settings')
 
-app.config.from_object('package_app_settings')
+pg_host    = getenv('PG_HOST')
+pg_db      = getenv('PG_DB')
+pg_user    = getenv('PG_USER')
+pg_pass    = getenv('PG_PASSWORD')
+db_connection_str = "host='{}' dbname='{}' user='{}' password='{}'".format(
+    pg_host, pg_db, pg_user, pg_pass)
+
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+
+@app.teardown_request
+def teardown_request(exception):
+    if hasattr(g, 'db'):
+        close_db()
 
 
 def connect_db():
+    """ Connects to the DB and returns the db connection
     """
-    Connects to the DB and creates a cursor
-    """
-    pass
+    return psycopg2.connect(db_connection_str)
 
 
 def close_db():
+    """ Closes DB connections and cursor
     """
-    Closes DB connections and cursor
-    """
-    pass
+    g.db.close()
+
+
 
 
 def logged_in():
-    """
-    Confirms that a user has a package
+    """ Confirms that a user has a package
     """
     if has_package():
         return redirect(url_for('appointment'))
@@ -38,11 +53,19 @@ def logged_in():
         return redirect(url_for('home'))
 
 
-def has_package():
-    """
-    Determines if a user has a package waiting for them.
+def has_package(uni):
+    """ Determines if a user has a package waiting for them.
+        Needs to implement a request to the Ricoh database.
     """
     pass
+
+
+@app.route('/appointment')
+def appointment():
+    appointments = queries.next_week_appointments(g.db)
+
+    return redirect(url_for('home'))
+    #return render_template('choose.html')
 
 
 @app.route('/')
@@ -51,5 +74,5 @@ def home():
 
 
 if __name__ == '__main__':
-    setup_db.setup()
     app.run()
+
