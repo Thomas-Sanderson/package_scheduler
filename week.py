1b2936b69fb0cdcd557e87681c9613df188e7429
Nate Brennand <natebrennand@gmail.com>
2013-06-29 17:24:18 -0400

    update week.py to read allotments

--- week.py
+++ week.py
@@ -1,8 +1,14 @@
 
-from config import allotment
 from datetime import date as dt_date
+from datetime import timedelta as dt_delta
 import queries
 
+this_week = (dt_date.today() - dt_delta(days=dt_date.today().weekday()))
+this_week_str = (dt_date.today() - dt_delta(days=dt_date.today().weekday())).strftime("%m-%d-%Y")
+next_week_str = (this_week + dt_delta(weeks=1)).strftime("%m-%d-%Y")
+
+this_week = getattr(__import__('config.allotment', fromlist=[this_week_str]), this_week_str)
+next_week = getattr(__import__('config.allotment', fromlist=[next_week_str]), next_week_str)
 
 class Week:
 
@@ -19,13 +25,13 @@ class Week:
             for j in range(24):
                 week_array[i].append([])
                 for k in range(4):
-                    week_array[i][j].append(allotment.current_week[i][j])
+                    week_array[i][j].append(this_week.week[i][j])
         for i in range(7, 14):   # second week
             week_array.append([])
             for j in range(24):
                 week_array[i].append([])
                 for k in range(4):
-                    week_array[i][j].append(allotment.next_week[i % 7][j])
+                    week_array[i][j].append(next_week.week[i % 7][j])
 
         # limiting array to only the next 4 days
         self.now = dt_date.today().weekday()+1
