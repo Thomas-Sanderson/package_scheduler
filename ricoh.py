8a264e9c7ac80afac8adc55ce9492ee38ac73646
Christian Moscardi <clm2183@columbia.edu>
2013-06-29 16:38:26 -0400

    playing with the API jank

---
+++ ricoh.py
@@ -0,0 +1,11 @@
+import urllib
+import urllib2
+
+#GET request, needs 1 param via string.format() function
+
+ID_URL = "https://columbiauniversity.ikontrac.com/external/kiosks/mail/pickup/getEmployeeId.cfm?loginType=custom09&id={}"
+
+#POST with studentid={uni} as param
+UNI_URL = "https://columbiauniversity.ikontrac.com/external/kiosks/mail/pickup/index.cfm?"
+
+
