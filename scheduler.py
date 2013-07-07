4e502b9926fbe7f490b66229d71d1de8c1a99587
Nate Brennand <natebrennand@gmail.com>
2013-07-07 00:32:40 -0400

    change comment style

--- scheduler.py
+++ scheduler.py
@@ -6,7 +6,6 @@ import week
 import psycopg2
 from datetime import date, datetime, timedelta
 from os import getenv
-
 import ricoh
 
 app = Flask(__name__)
@@ -18,7 +17,6 @@ pg_user    = getenv('PG_USER')
 pg_pass    = getenv('PG_PASSWORD')
 db_connection_str = "host='{}' dbname='{}' user='{}' password='{}'".format(
     pg_host, pg_db, pg_user, pg_pass)
-
 
 
 @app.before_request
@@ -34,52 +32,51 @@ def teardown_request(exception):
 
 
 def connect_db():
-    """ Connects to the DB and returns the db connection
-    """
+    # Connects to the DB and returns the db connection
     return psycopg2.connect(db_connection_str)
 
 
 def close_db():
-    """ Closes DB connections and cursor
-    """
+    # Closes DB connections and cursor
     g.db.close()
 
 
 def has_package(uni):
-    """ Determines if a user has a package waiting for them.
-        Needs to implement a request to the Ricoh database.
-    """
     return ricoh.package_info(uni)
+    # Determines if a user has a package waiting for them.
+    # Needs to implement a request to the Ricoh database.
 
 def room_for_appointment(week, timeslot):
-    """ Makes a last chance check that there is still a slot available for the
-        chosen timeslot.
-        timeslot : datetime object
-    """
+    # Makes a last chance check that there is still a slot available for the
+    # chosen timeslot.
+    # timeslot : datetime object
     pass
 
 
 @app.route('/appointment', methods=['GET', 'POST'])
 def appointment():
+
+    if 'package' not in session or session['package'] == False:
+        print 'oops'
+        flash("Sorry, you can't access the appointment page unless you have"+
+            " a package waiting for you.")
+        return render_template('home.html')
+
+    # Loads the main screen that displays all of the timeslots. Their
+    # availablilities will be represented in a heat map fashion.
     if request.method == 'GET':
-        """ Loads the main screen that displays all of the timeslots. Their
-            availablilities will be represented in a heat map fashion.
-        """
         first = date.today()+timedelta(days=1)
         days = []
         for i in range(4):
             days.append(first+timedelta(days=i))
-        """ PASSED IN VALUES
-            days = 4 relevant dates
-            week = week object
-        """
+
+        # PASSED IN VALUES: days = 4 relevant dates, week = week object
         return render_template('choose.html', days=days, week=g.week.slots)
 
+    # Logs the appointment and inserts the new appointment to the database.
+    # Before logging must call room_for_appointments() to avoid overbooking.
     else:
-        """ Logs the appointment and inserts the new appointment to the database.
-            Before logging must call room_for_appointments() to avoid overbooking.
-        """
-        # hacky hacky hacky, TODO: refactor
+        # TODO: refactor
         choice = request.form["choice"].replace('[','').replace(']','')
         choice = choice.replace('(','').replace(')','').split(',')
         for i in range(len(choice)):
