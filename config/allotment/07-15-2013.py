db8b9ef003bdf0e538b242df5e5c7a118c4de3e6
Nate Brennand <natebrennand@gmail.com>
2013-07-08 09:39:56 -0400

    add new week's file

---
+++ config/allotment/07-15-2013.py
@@ -0,0 +1,37 @@
+"""
+fake data for testing purposes
+"""
+
+# midnight - 8 AM
+# 9 AM - 4 PM
+# 5 PM - 11 PM
+
+week = [  # Monday
+    [None, None, None, None, None, None, None, None,
+     8,  7,  4,  2,  0,  0,  0,  2,
+     5,  7,  9,  9,  None, None, None, None],
+    # Tuesday
+    [None, None, None, None, None, None, None, None,
+     8,  7,  4,  2,  0,  0,  0,  2,
+     5,  7,  9,  9,  None, None, None, None],
+    # Wednesday
+    [None, None, None, None, None, None, None, None,
+     8,  7,  4,  2,  0,  0,  0,  2,
+     5,  7,  9,  9,  None, None, None, None],
+    # Thursday
+    [None, None, None, None, None, None, None, None,
+     8,  7,  4,  2,  0,  0,  0,  2,
+     5,  7,  9,  9,  None, None, None, None],
+    # Friday
+    [None, None, None, None, None, None, None, None,
+     8,  7,  4,  2,  0,  0,  0,  2,
+     5,  7,  9,  9,  None, None, None, None],
+    # Saturday
+    [None, None, None, None, None, None, None, None,
+     None, None, None, None, None, None, None, None,
+     None, None, None, None, None, None, None, None],
+    # Sunday
+    [None, None, None, None, None, None, None, None,
+     None, None, None, None, None, None, None, None,
+     None, None, None, None, None, None, None, None]]
+
